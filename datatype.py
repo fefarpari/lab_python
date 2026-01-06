d305ebf6e92c9530f7fb2392fa2a906a258aa417
fefarpari <fefarpari67@gmail.com>
2026-01-06 08:20:53 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,28 @@
+x="hello world"
+print(x)
+x=25
+print(x)
+x=25.5
+print(x)
+x=1j
+print(x)
+x=["apple","banana","cherry"]
+print(x)
+x=("apple","banana","cherry")
+print(x)
+x=range(6)
+print(x)
+x={"name":"john", "age":36}
+print(x)
+x=({"apple","banana","cherry"})
+print(x)
+x=True
+print(x)
+x=b"hello"
+print(x)
+x=bytearray(5)
+print(x)
+x=memoryview (bytes(5))
+print(x)
+x=None
+print(x)
