14a37c983201caa4519f316ca6bd8b0570bdb201
fefarpari <fefarpari67@gmail.com>
2026-01-27 08:20:54 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,49 @@
+#arithmetic operator
+
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#assignment operator
+x=10
+print(x)
+
+#unary operator
+a=5
+print(-a)
+
+#relational operator
+a=7
+b=2
+print(a<b)
+
+#logical operator
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#boolean
+is_pass=True
+print(is_pass)
+
+#bitwise operator
+a=4
+b=3
+print(a&b)
+print(a/b)
+
+#membership operator
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 is not numbers)
+
+#identity operator
+x=10
+y=10
+print(x is y)
+print(x is not y)
