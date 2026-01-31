5a445f134109ab684f825b1b80a44cefe19f4d04
fefarpari <fefarpari67@gmail.com>
2026-01-31 09:21:23 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+name=input(("enter your name : "))
+print("hello",name)
+
+num=input(("enter number : "))
+print("number=",num)
+
+num=input("enter number : ")
+print("float number : ",num)
+
+first=input("enter first word : ")
+second=input("enter secound word : ")
+print("you typed : ",first,second)
+
+print("A","B","C")
+print("A","B","C",sep="-")
+
+name=input("enter your name : ")
+marks=input("enter your marks : ")
+print("student and marks : ",name,marks)
