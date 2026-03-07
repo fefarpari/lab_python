0011e94f51f8b9fb4cb83cabafb9fc95c862c38d
fefarpari <fefarpari67@gmail.com>
2026-03-07 08:26:10 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,19 @@
+#positive index
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])
+
+#negative index
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
+
+#modifying element using index
+from array import array
+arr=array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)
